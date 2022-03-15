0ac51aa6a2739a683c4c0db135f1b28202bbd805
Mikhail_Myadelets <mrr.myadelets@mail.ru>
2022-03-15 20:23:41 +0300

    first_commit

---
+++ lib/countries.h
@@ -0,0 +1,27 @@
+#ifndef COUNTRIES_H
+#define COUNTRIES_H
+
+typedef struct Country
+{
+	float square;
+	int population;
+	char *capital;
+} Country;
+
+
+bool create(FILE * input, Country **data, size_t *size);
+
+bool fill(FILE * input, Country *const data, size_t num);
+
+void show(FILE *output, const Country const *data, size_t size);
+
+void swap(Country *const a, Country *const b);
+
+int partition(Country *const data, size_t size);
+
+void quick_sort(Country *const data, size_t size);
+
+void destroy(Country *data, size_t size);
+
+#endif // COUNTRIES_H
+
