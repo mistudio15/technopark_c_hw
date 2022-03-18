2214ce6c0c6ce11179fbf619967b8a12cd57170b
Mikhail Myadelets <71098937+mistudio15@users.noreply.github.com>
2022-03-18 19:53:39 +0300

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -20,6 +20,15 @@ pip install cpplint
 sudo apt-get -q install cppcheck
 sudo apt-get install libc6-dbg
 sudo apt install valgrind
+
+apt-get update && \
+	apt-get upgrade -y &&  \
+	DEBIAN_FRONTEND=noninteractive apt-get install -y \
+		build-essential \
+		valgrind \
+		curl \
+		clang && \
+	rm -fr /var/lib/apt/lists/*
             
 apt-get update && sudo apt-get install -y valgrind
 apt-get update && sudo apt-get install -y cmake
